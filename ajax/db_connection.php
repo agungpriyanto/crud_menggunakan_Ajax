a75c0082229f023aa3618b75ff6c0b3a5bff24b6
agungpriyanto <37022113+agungpriyanto@users.noreply.github.com>
2018-06-04 06:17:18 +0700

    Add files via upload

---
+++ ajax/db_connection.php
@@ -0,0 +1,12 @@
+<?php
+
+// Connection variables 
+$host = "localhost"; // MySQL host name eg. localhost
+$user = "root"; // MySQL user. eg. root ( if your on localserver)
+$password = ""; // MySQL user password  (if password is not set for your root user then keep it empty )
+$database = "test"; // MySQL Database name
+
+// Connect to MySQL Database 
+$db = mysqli_connect($host, $user, $password,$database);
+
+?>
